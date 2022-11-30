d61d451a901ae08bdcfc046e43a9fbe5bb9111e3
Kyle Tickner <ktickner.91@gmail.com>
2022-11-30 13:31:09 +0100

    chore(init): set up base theme and css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <header className="App-header">
         <p>Clevershuttle test</p>
       </header>
-    </div>
+    </ThemeProvider>
   );
 }
 
